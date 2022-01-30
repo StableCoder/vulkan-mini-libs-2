705622ac465b9d50acd8eb5cd5f2ac95574b43c5
George Cave <gcave@stablecoder.ca>
2022-01-29 22:24:10 -0500

    Updated copyright year
    Updated for 2022.

--- include/vk_struct_cleanup.h
+++ include/vk_struct_cleanup.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2021 George Cave - gcave@stablecoder.ca
+    Copyright (C) 2021-2022 George Cave - gcave@stablecoder.ca
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
