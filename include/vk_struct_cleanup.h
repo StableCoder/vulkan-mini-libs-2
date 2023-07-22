58fcd7cb7e11cd37f821db6ffa4de672690a9adc
George Cave <gcave@stablecoder.ca>
2023-07-22 11:24:37 -0400

    Add Vulkan v1.3.259 support

--- include/vk_struct_cleanup.h
+++ include/vk_struct_cleanup.h
@@ -32,13 +32,13 @@ extern "C" {
 #ifdef __cplusplus
 static_assert(VK_HEADER_VERSION >= 72,
               "VK_HEADER_VERSION is from before the minimum supported version of v72.");
-static_assert(VK_HEADER_VERSION <= 258,
-              "VK_HEADER_VERSION is from after the maximum supported version of v258.");
+static_assert(VK_HEADER_VERSION <= 259,
+              "VK_HEADER_VERSION is from after the maximum supported version of v259.");
 #else
 _Static_assert(VK_HEADER_VERSION >= 72,
                "VK_HEADER_VERSION is from before the minimum supported version of v72.");
-_Static_assert(VK_HEADER_VERSION <= 258,
-               "VK_HEADER_VERSION is from after the maximum supported version of v258.");
+_Static_assert(VK_HEADER_VERSION <= 259,
+               "VK_HEADER_VERSION is from after the maximum supported version of v259.");
 #endif
 
 void cleanup_vk_struct(void const *pData);
