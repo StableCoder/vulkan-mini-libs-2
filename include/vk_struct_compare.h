dd256a502fee16d4c53b71a6595d8da26307f5c4
George Cave <gcave@stablecoder.ca>
2024-07-13 21:04:40 -0400

    Add Vulkan v1.3.290 support

--- include/vk_struct_compare.h
+++ include/vk_struct_compare.h
@@ -39,13 +39,13 @@ extern "C" {
 #ifdef __cplusplus
 static_assert(VK_HEADER_VERSION >= 72,
               "VK_HEADER_VERSION is from before the minimum supported version of v72.");
-static_assert(VK_HEADER_VERSION <= 289,
-              "VK_HEADER_VERSION is from after the maximum supported version of v289.");
+static_assert(VK_HEADER_VERSION <= 290,
+              "VK_HEADER_VERSION is from after the maximum supported version of v290.");
 #else
 _Static_assert(VK_HEADER_VERSION >= 72,
                "VK_HEADER_VERSION is from before the minimum supported version of v72.");
-_Static_assert(VK_HEADER_VERSION <= 289,
-               "VK_HEADER_VERSION is from after the maximum supported version of v289.");
+_Static_assert(VK_HEADER_VERSION <= 290,
+               "VK_HEADER_VERSION is from after the maximum supported version of v290.");
 #endif
 
 bool compare_VkOffset2D(VkOffset2D const *s1, VkOffset2D const *s2);
