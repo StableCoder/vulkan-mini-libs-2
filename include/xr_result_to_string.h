6d6e83ca6e05fb07ce61463989668d5bdd9e1cfe
George Cave <gcave@stablecoder.ca>
2024-08-23 07:27:35 -0700

    Add OpenXR v1.0.40 support

--- include/xr_result_to_string.h
+++ include/xr_result_to_string.h
@@ -32,13 +32,13 @@ extern "C" {
 #ifdef __cplusplus
 static_assert((XR_CURRENT_API_VERSION & 0xffffffffULL) >= 0,
               "openxr header version is from before the minimum supported version of v0.");
-static_assert((XR_CURRENT_API_VERSION & 0xffffffffULL) <= 38,
-              "openxr header version is from after the maximum supported version of v38.");
+static_assert((XR_CURRENT_API_VERSION & 0xffffffffULL) <= 40,
+              "openxr header version is from after the maximum supported version of v40.");
 #else
 _Static_assert((XR_CURRENT_API_VERSION & 0xffffffffULL) >= 0,
                "openxr header version is from before the minimum supported version of v0.");
-_Static_assert((XR_CURRENT_API_VERSION & 0xffffffffULL) <= 38,
-               "openxr header version is from after the maximum supported version of v38.");
+_Static_assert((XR_CURRENT_API_VERSION & 0xffffffffULL) <= 40,
+               "openxr header version is from after the maximum supported version of v40.");
 #endif
 
 /// Returns a string representing the given VkResult parameter. If there is no known representation,
