6f15289cd3184f5da397b0f32fee890deaae49a9
George Cave <gcave@stablecoder.ca>
2025-03-08 06:53:08 -0500

    Add OpenXR v1.1.46 support

--- include/xr_result_to_string.h
+++ include/xr_result_to_string.h
@@ -37,13 +37,13 @@ _Static_assert((XR_CURRENT_API_VERSION & 0xffffffffULL) >= 0,
                "XR_CURRENT_API_VERSION is lower than the minimum supported version (v0)");
 #endif
 
-#if (XR_CURRENT_API_VERSION & 0xffffffffULL) > 45
+#if (XR_CURRENT_API_VERSION & 0xffffffffULL) > 46
 #if _MSC_VER
 #pragma message(                                                                                   \
     __FILE__                                                                                       \
-    ": warning: XR_CURRENT_API_VERSION is higher than what the header fully supports (v45)")
+    ": warning: XR_CURRENT_API_VERSION is higher than what the header fully supports (v46)")
 #else
-#warning "XR_CURRENT_API_VERSION is higher than what the header fully supports (v45)"
+#warning "XR_CURRENT_API_VERSION is higher than what the header fully supports (v46)"
 #endif
 #endif
 
