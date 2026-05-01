edf207e75390610a1358208fc04179c14c67068b
George Cave <gcave@stablecoder.ca>
2026-05-01 07:41:15 -0400

    Add OpenXR v1.1.59 support

--- include/xr_result_to_string.h
+++ include/xr_result_to_string.h
@@ -37,13 +37,13 @@ _Static_assert((XR_CURRENT_API_VERSION & 0xffffffffULL) >= 0,
                "XR_CURRENT_API_VERSION is lower than the minimum supported version (v0)");
 #endif
 
-#if (XR_CURRENT_API_VERSION & 0xffffffffULL) > 58
+#if (XR_CURRENT_API_VERSION & 0xffffffffULL) > 59
 #if _MSC_VER
 #pragma message(                                                                                   \
     __FILE__                                                                                       \
-    ": warning: XR_CURRENT_API_VERSION is higher than what the header fully supports (v58)")
+    ": warning: XR_CURRENT_API_VERSION is higher than what the header fully supports (v59)")
 #else
-#warning "XR_CURRENT_API_VERSION is higher than what the header fully supports (v58)"
+#warning "XR_CURRENT_API_VERSION is higher than what the header fully supports (v59)"
 #endif
 #endif
 
@@ -363,6 +363,8 @@ char const *XrResult_to_string(XrResult result) {
     return "XR_ERROR_WORLD_MESH_DETECTOR_SPACE_NOT_LOCATABLE_ML";
   case 1000482000:
     return "XR_ERROR_FACIAL_EXPRESSION_PERMISSION_DENIED_ML";
+  case 1000528000:
+    return "XR_BOUNDARY_VISIBILITY_SUPPRESSION_NOT_ALLOWED_META";
   case -1000571001:
     return "XR_ERROR_COLOCATION_DISCOVERY_NETWORK_FAILED_META";
   case -1000571002:
